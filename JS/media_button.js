fab3d738e131fd459ad169fa29c7a0c12fc9d6fb
Desktop <bulzevalexandr@yandex.ru>
2024-03-24 23:17:33 +0300

    Added script for media buttons

---
+++ JS/media_button.js
@@ -0,0 +1,28 @@
+var buttons = document.getElementsByClassName('media-item');
+if(buttons.length > 0) {
+    buttons[0].addEventListener('click', function() {
+        window.open("https://web.telegram.org/k/")
+    })
+    buttons[1].addEventListener('click', function() {
+        window.open("https://www.youtube.com/")
+    })
+    buttons[2].addEventListener('click', function() {
+        window.open("https://vk.com/feed")
+    })
+    buttons[3].addEventListener('click', function() {
+        window.open("https://www.whatsapp.com/")
+    })
+}
+
+var buttons2 = document.getElementsByClassName('footer-media');
+if(buttons2.length > 0) {
+    buttons2[0].addEventListener('click', function() {
+        window.open("https://web.telegram.org/k/")
+    })
+    buttons2[1].addEventListener('click', function() {
+        window.open("https://www.youtube.com/")
+    })
+    buttons2[2].addEventListener('click', function() {
+        window.open("https://vk.com/feed")
+    })
+}
